97d2370492bedbf047371e2b8cc9e76df8a98007
Volodymyr Bondar <2005volka@gmail.com>
2024-10-15 13:06:08 +0300

    feat: added boxes to contact

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -2,26 +2,32 @@ import { useTranslations } from "next-intl";
 import MagicButton from "../ui/MagicButton";
 import Link from "next/link";
 import { FaLocationArrow } from "react-icons/fa";
+import { Boxes } from "../ui/BackgroundBoxes";
 
 function Contact() {
   const t = useTranslations("Contact");
 
   return (
-    <section className="mb-32 flex items-center justify-center gap-3 flex-col">
-      <h2 className="text-5xl font-bold max-w-[50vw] text-center">
-        {t("title")}
-      </h2>
-      <p className="text-base">{t("reach-out")}</p>
+    <section className="mb-32 h-96 w-full bg-black-100 relative">
+      <div className="flex items-center justify-center gap-3 flex-col absolute inset-0 w-full h-full bg-black-100 z-20 [mask-image:radial-gradient(white,transparent)]">
+        <Boxes />
 
-      <Link
-        href="https://drive.google.com/file/d/1hhQDKHFjVJh8Tj3Qsc8iTDswi-K7VF-Z/view?usp=sharing"
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        <MagicButton icon={<FaLocationArrow />} position="right">
-          {t("btn")}
-        </MagicButton>
-      </Link>
+        <h2 className="text-5xl font-bold max-w-[50vw] text-center relative z-20">
+          {t("title")}
+        </h2>
+        <p className="text-base relative z-20">{t("reach-out")}</p>
+
+        <Link
+          href="https://drive.google.com/file/d/1hhQDKHFjVJh8Tj3Qsc8iTDswi-K7VF-Z/view?usp=sharing"
+          rel="noopener noreferrer"
+          target="_blank"
+          className="relative z-20"
+        >
+          <MagicButton icon={<FaLocationArrow />} position="right">
+            {t("btn")}
+          </MagicButton>
+        </Link>
+      </div>
     </section>
   );
 }
