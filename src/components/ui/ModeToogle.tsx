fdf2ed38f77c5c4c3b866485ca0c2c35a2e8b4b8
Volodymyr Bondar <2005volka@gmail.com>
2024-10-17 20:39:57 +0300

    feat: updated mode toggle and added work experience to ua version

--- src/components/ui/ModeToogle.tsx
+++ src/components/ui/ModeToogle.tsx
@@ -4,36 +4,24 @@ import { FaMoon, FaSun } from "react-icons/fa";
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/Button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/DropdownMenu";
+import { useState } from "react";
 
 export function ModeToggle() {
   const { setTheme } = useTheme();
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  function handleToggle() {
+    if (isDarkMode) setTheme("dark");
+    else setTheme("light");
+
+    setIsDarkMode((prev) => !prev);
+  }
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <FaSun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <FaMoon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="center">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Button variant="outline" size="icon" onClick={handleToggle}>
+      <FaSun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <FaMoon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
   );
 }
